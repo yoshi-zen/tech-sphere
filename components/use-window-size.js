4c1ce32333b095acc680f24324d26b0ddb26dcc7
yoshi_zen <yoshi180705@gmail.com>
2023-10-05 06:48:46 +0900

    諸々微調整

---
+++ components/use-window-size.js
@@ -0,0 +1,17 @@
+import { useState, useLayoutEffect } from 'react'
+
+export default function useWindowSize() {
+  const [size, setSize] = useState([0, 0])
+
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth, window.innerHeight])
+    }
+
+    window.addEventListener('resize', updateSize)
+    updateSize()
+
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+  return size
+}
