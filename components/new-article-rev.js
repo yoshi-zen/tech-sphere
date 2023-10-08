a247ff116d8bf6832dcf2a7e99a790f58952b20e
yoshi_zen <yoshi180705@gmail.com>
2023-10-09 00:28:36 +0900

    微調整

--- components/new-article-rev.js
+++ components/new-article-rev.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import NewArticleCard from './new-article-card'
 import styles from '@/styles/new-article-rev.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
-export default function NewArticleRev({ posts, main = false }) {
+export default function NewArticleRev({ posts, main = false, amount = 6 }) {
   return (
-    <section style={{ width: '100%' }}>
+    <section className={styles.container}>
       <h1 className={styles.title1}>新着記事</h1>
       <div className={main ? styles.main : styles.sub}>
-        {posts.slice(0, 6).map(({ title, slug, eyecatch, author }) => (
+        {posts.slice(0, amount).map(({ title, slug, eyecatch, author }) => (
           <NewArticleCard
             key={slug}
             slug={slug}
@@ -17,6 +20,13 @@ export default function NewArticleRev({ posts, main = false }) {
           />
         ))}
       </div>
+      <Link
+        href="/articles"
+        style={{ fontWeight: 700, display: 'block', textAlign: 'right' }}
+      >
+        すべての記事を見る　
+        <FontAwesomeIcon icon={faChevronRight} />
+      </Link>
     </section>
   )
 }
