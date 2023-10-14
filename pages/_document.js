05b9b6213a065d99bec8fddeb2007b3cc9968379
yoshi_zen <yoshi180705@gmail.com>
2023-10-14 10:57:50 +0900

    Googleアナリティクス生成

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 import { siteMeta } from 'lib/site-constants'
+import GoogleAnalytics from '@/components/google-analytics'
 const { siteLang } = siteMeta
 
 export default function Document() {
   return (
     <Html lang={siteLang}>
       <Head>
+        <GoogleAnalytics />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.css"
