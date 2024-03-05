e2fb7789a319c3da582f086b4c4d5ad966569158
yoshi_zen <yoshi180705@gmail.com>
2024-03-05 23:36:10 +0900

    ts系でちょいちょい怒られるな。不安定だ

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
